ffe12b8eafb07d06accbe4b99808d896839bc2cd
Alexander Shustik <Shustik@Air-Alexander.Dlink>
2016-02-17 13:56:57 +0200

    add css/js

--- views/layouts/basic.php
+++ views/layouts/basic.php
@@ -1,15 +1,25 @@
-<?
+<?php
+	use app\assets\AppAsset;
 	// @var $content string
+	// @var $this \yii\web\View
+	AppAsset::register($this);
+	$this->beginPage();
 ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
 	<meta charset="<?= Yii::$app->charset ?>">
 	<title><?= Yii::$app->name ?></title>
+	<?php $this->head(); ?>
 </head>
 <body>
+	<?php $this->beginBody(); ?>
 	<p>Шапка</p>
 	<?= $content ?>
 	<p>Подвал</p>
+	<?php $this->endBody(); ?>
 </body>
 </html>
+<?php
+	$this->endPage();
+?>
