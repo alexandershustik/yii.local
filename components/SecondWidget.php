8a40bfba7a29ee914ee421fd6b49fa3a47688dc5
Alexander Shustik <Shustik@Air-Alexander.Dlink>
2016-02-17 23:48:17 +0200

    widget work

---
+++ components/SecondWidget.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\components;
+
+use yii\base\Widget;
+
+class SecondWidget extends Widget
+{
+	public function init(){
+		parent::init();
+		ob_start();
+	}
+
+	public function run(){
+		$content = ob_get_clean();
+		return $this->render('second',
+			[
+				'content' => $content
+			]);
+	}
+}
